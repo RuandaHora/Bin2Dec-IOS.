1ea1b0dd9d34cbbfb28cf8569fd6c8775011480d
Ruan da Hora <ruanpablosnake@gmail.com>
2022-12-30 00:03:39 -0300

    Initial Commit

---
+++ Bin2Dec-IOS/Bin2Dec_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bin2Dec_IOSApp.swift
+//  Bin2Dec-IOS
+//
+//  Created by Marcelo Hora on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Bin2Dec_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
