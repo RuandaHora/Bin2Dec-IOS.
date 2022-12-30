96fce4434eb91d4355e964cfa3970c9f1dc169ca
Ruan da Hora <ruanpablosnake@gmail.com>
2022-12-30 00:22:41 -0300

    Update

--- Bin2Dec-IOS/Loading Page.swift
+++ Bin2Dec-IOS/Loading Page.swift
@@ -17,7 +17,7 @@ struct Loadingpage : View {
             Color.black.ignoresSafeArea()
              
             VStack{
-                Image(systemName: "d.circle")
+                Image(systemName: "star.circle")
                     .foregroundStyle(.white, .tertiary)
                     .font(.system(size: 90))
                     .rotationEffect(.degrees(isAnimated ? 360 : 0))
