e07e61741411301cc52d69509dceec7d196f3085
Ruan da Hora <ruanpablosnake@gmail.com>
2022-12-31 03:20:28 -0300

    Update Converter

--- Bin2Dec-IOS/Conversor.swift
+++ Bin2Dec-IOS/Conversor.swift
@@ -20,111 +20,116 @@ struct ContentView: View {
     @State var numero2 = ""
     
     var body: some View {
-       
-        VStack {
+        ZStack{
+            RoundedRectangle(cornerRadius: 0)
+            .fill(Color("CustomColor"))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea()
             
-            HStack{
-                Text("Binary to Decimal Converter")
-                    .font(.headline)
-                    .foregroundColor(.primary)
-            }
-            HStack{
-                TextField("Binario", text: self.$email)
-                    .foregroundColor(Color.black)
-                    .frame(width: 280, height: 35)
-                    .background(Color.white)
-                    .cornerRadius(5)
-                
-                Button(action: {}) {
-                    Image("back")
-                        
-                        .frame(width: 75, height: 30)
-                        .background(Color("Icones"))
-                        .cornerRadius(5)
+            VStack {
+                HStack{
+                    Text("                                                            ")
+                    Button(action: {}) {
+                        Image(systemName:"house.circle")
+                    }
                 }
-            }
-            HStack{
-                TextField("Decimal", text: self.$password)
-                    .foregroundColor(.black)
-                    .frame(width: 280, height: 35)
-                    .background(Color.white)
-                    .cornerRadius(5)
-                
-                Button(action: {}) {
-                    Image("copy")
+                HStack{
+                    Text("Binary to Decimal Converter")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                    Text("                               ")
+                }
+                HStack{
+                    TextField("Binario", text: self.$email)
+                        .foregroundColor(Color.black)
+                        .frame(width: 280, height: 35)
+                        .background(Color("CustomColor"))
+                        .cornerRadius(5)
                     
-                        .frame(width: 75, height: 30)
-                        .background(Color("Icones"))
+                    Button(action: {}) {
+                        Image("back")
+                        
+                            .frame(width: 75, height: 30)
+                            .background(Color("Icones"))
+                            .cornerRadius(5)
+                    }
+                }
+                HStack{
+                    TextField("Decimal", text: self.$password)
+                        .foregroundColor(.black)
+                        .frame(width: 280, height: 35)
+                        .background(Color("CustomColor"))
                         .cornerRadius(5)
+                    
+                    Button(action: {}) {
+                        Image("copy")
+                        
+                            .frame(width: 75, height: 30)
+                            .background(Color("Icones"))
+                            .cornerRadius(5)
+                    }
+                    
+                }
+                HStack{
+                    Text("Enter a binary number: ")
+                        .font(.headline)
+                        .foregroundColor(.primary)
                 }
                 
-            }
-            HStack{
-                Text("Enter a binary number: ")
-                    .font(.headline)
-                    .foregroundColor(.primary)
-            }
-            
-            HStack{
-            
-                TextField("0", text: self.$numero1)
+                HStack{
+                    
+                    TextField("0", text: self.$numero1)
                         .font(.headline)
                         .foregroundColor(.blue)
-                        .frame(width: 80, height: 35)
+                        .frame(width: 70, height: 30)
                         .background(Color("Color"))
-                        .cornerRadius(10)
-                            
-            }
-            
-            HStack{
-               
-                TextField("0", text: self.$numero2)
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .frame(width: 80, height: 35)
-                        .background(Color("Color"))
-                        .cornerRadius(10)
-                            
-            }
-
-            HStack{
-                Button(action: {}) {
-                    Text("Convert")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(width: 200, height: 35)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-
+                        .cornerRadius(5)
+                    
                 }
-                            
-            }
-            
-            Spacer()
-            HStack{
-                Button(action: {}) {
-                    Text("by Ruan da Hora")
-                    .foregroundColor(Color.white)
-                    .padding(1)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .scaleEffect(crescendo)
-                    .animation(
-                    Animation
-                        .easeIn(duration: 3)
-                        .repeatCount(3)
-                        .delay(2)
-                    )
+                HStack{
+                    
+                    TextField("0", text: self.$numero2)
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .frame(width: 70, height: 30)
+                        .background(Color("Color"))
+                        .cornerRadius(5)
+                    
+                }
+                HStack{
+                    Button(action: {}) {
+                        
+                        Text("Convert")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 35)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                        
+                    }
+                }
+                
+                Spacer()
+                HStack{
+                    Button(action: {}) {
+                        Text("by Ruan da Hora")
+                            .foregroundColor(Color.white)
+                            .padding(1)
+                            .background(Color("Color"))
+                            .cornerRadius(10)
+                            .scaleEffect(crescendo)
+                            .animation(
+                                Animation
+                                    .easeIn(duration: 3)
+                                    .repeatCount(3)
+                                    .delay(2))
+                    }
                 }
             }
             .onAppear{
                 self.crescendo -= 0.1
             }
         }
-        RoundedRectangle(cornerRadius: 0)
-        .fill(Color("CustomColor"))
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .ignoresSafeArea()
     }
 }
 
